925366197004fe67affc5e35e471ebd4e77a3b5b
Phi Dinh <phi.dinh@cyberlogitec.com>
2023-05-10 15:02:04 +0700

    test component

---
+++ src/components/ui/tests/button.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import Button from '../Button';
+
+describe('Button component', () => {
+  test('renders button with correct text', () => {
+    const buttonText = 'Click me';
+    render(<Button>{buttonText}</Button>);
+
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveTextContent(buttonText);
+  });
+
+  test('calls onClick handler when clicked', () => {
+    const onClickMock = jest.fn();
+    render(<Button onClick={onClickMock}>Click me</Button>);
+
+    const buttonElement = screen.getByRole('button');
+    buttonElement.click();
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
